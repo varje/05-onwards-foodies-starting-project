b8328c946a1a293722c419c33bab7ee690a6031f
varje <varjekivila@gmail.com>
2025-09-10 11:26:21 +0300

    add header

---
+++ components/main-header.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import logoImg from '@assets/logo.png';
+import classes from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <img src={logoImg.src} alt="A plate of food on it." />
+        NextLevel Food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+            <li><Link href="/meals">Browse meals</Link></li>
+             <li><Link href="/community">Foodies community</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

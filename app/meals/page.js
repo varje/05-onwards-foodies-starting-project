d4ea69cc64a8e5fe1463a741ff8e946fe5734558
varje <varjekivila@gmail.com>
2025-09-11 18:03:49 +0300

    create empty meals grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}></header>
+      <h1>
+        Delicious meals, created{' '}
+        <span className={classes.highlgiht}>by you</span>
+      </h1>
+      <p>
+        Choose your favourite recipe and cook it yourself. It is easy and fun!
+      </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">Share Tour Favourite Recipe!</Link>
+      </p>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

9551b195be437fc0a6bc1e40df262f5e7ab55a1f
varje <varjekivila@gmail.com>
2025-09-12 15:59:46 +0300

    add loading screen to meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}></header>
@@ -19,7 +24,9 @@ export default async function MealsPage() {
         <Link href="/meals/share">Share Tour Favourite Recipe!</Link>
       </p>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={ <p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
